4507fdcb460d8dc8ff7e987cb01e41aeb18f7e23
Tiffany White <tiffanywhitedev412@gmail.com>
2017-08-18 21:44:29 -0400

    Adds Express files, views and partials

---
+++ v1/app.js
@@ -0,0 +1,27 @@
+let express = require("express");
+let app = express();
+
+app.set("view engine", "ejs");
+app.set("port", process.env.PORT || 3000);
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+var campgrounds = [
+  {name: "Bear Run", image:"https://s3.us-east-2.amazonaws.com/yelpcamp96/Bear.jpg"},
+  {name: "Camp Reilly", image:"https://s3.us-east-2.amazonaws.com/yelpcamp96/Camp_Reily.jpg"},
+  {name: "Pinchot Park", image:"https://s3.us-east-2.amazonaws.com/yelpcamp96/Pinchot.jpg"},
+];
+
+app.get("/campgrounds", function(req, res){
+  res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+// app.get("*", function(req, res) {
+//   res.render("404");
+// });
+
+app.listen(app.get("port"), function(){
+  console.log("App is listening on port 3000");
+});
