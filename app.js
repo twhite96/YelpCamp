f17325f4856445d11e8c4279e105d60258949a45
Tiffany White <tiffanywhitedev412@gmail.com>
2017-08-21 23:11:44 -0400

    Added mongoose, mongodb, updated header.ejs, added extra styles, added new.ejs and formatted form

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-let express = require("express");
-let app = express();
-let favicon = require("serve-favicon");
-let path = require("path");
+var express = require("express");
+var app = express();
+var favicon = require("serve-favicon");
+var path = require("path");
+var http = require("http");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.set("port", process.env.PORT || 3000);
 app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
@@ -19,6 +23,18 @@ var campgrounds = [
 
 app.get("/campgrounds", function(req, res){
   res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.get("/campgrounds/new", function(req, res){
+  res.render("new.ejs");
+});
+
+app.post("/campgrounds", function(req, res){
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name: name, image: image};
+  campgrounds.push();
+  res.redirect("/campgrounds");
 });
 
 // app.get("*", function(req, res) {
